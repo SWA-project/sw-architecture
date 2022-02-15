d3d867b21b1b7d050cd0652d8fc7a29d9c0f7964
ilkkamaksy <ijmakine@gmail.com>
2022-02-15 12:14:14 +0200

    feature: customer check handler

--- order-service/src/main/java/swa/order/config/OrderServiceConfig.java
+++ order-service/src/main/java/swa/order/config/OrderServiceConfig.java
@@ -1,26 +1,36 @@
 package swa.order.config;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
-import swa.order.dto.OrderCreatedEvent;
-import swa.order.dto.CreditCheckEvent;
-import swa.order.handler.EventConsumer;
 
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import swa.order.dto.CreditCheckEvent;
+import swa.order.dto.CustomerCheckEvent;
+import swa.order.dto.OrderCreatedEvent;
+import swa.order.handler.EventConsumer;
+import swa.order.handler.EventHandler;
 
 @Configuration
 public class OrderServiceConfig {
 
     private final EventConsumer<CreditCheckEvent> transactionEventConsumer;
+    private final EventHandler<CustomerCheckEvent, OrderCreatedEvent> customerCheckEventHandler;
 
     @Autowired
-    public OrderServiceConfig(EventConsumer<CreditCheckEvent> transactionEventConsumer) {
+    public OrderServiceConfig(
+    		EventConsumer<CreditCheckEvent> transactionEventConsumer,
+    		EventHandler<CustomerCheckEvent, OrderCreatedEvent> customerCheckEventHandler
+    		) {
         this.transactionEventConsumer = transactionEventConsumer;
+        this.customerCheckEventHandler = customerCheckEventHandler;
+
     }
 
     @Bean
@@ -29,13 +39,18 @@ public class OrderServiceConfig {
                 .multicast()
                 .directBestEffort();
     }
-
+  
     @Bean
     public Supplier<Flux<OrderCreatedEvent>> orderCreditEventPublisher(
             Sinks.Many<OrderCreatedEvent> publisher) {
         return publisher::asFlux;
     }
-
+    
+    @Bean
+    public Function<CustomerCheckEvent, OrderCreatedEvent> customerCheckEventProcessor() {
+        return customerCheckEventHandler::handleEvent;
+    }
+    
     @Bean
     public Consumer<CreditCheckEvent> transactionEventProcessor() {
         return transactionEventConsumer::consumeEvent;
--- order-service/src/main/java/swa/order/handler/EventConsumer.java
+++ order-service/src/main/java/swa/order/handler/EventConsumer.java
@@ -3,6 +3,5 @@ package swa.order.handler;
 import swa.order.dto.Event;
 
 public interface EventConsumer<T extends Event> {
-
     void consumeEvent(T event);
 }
