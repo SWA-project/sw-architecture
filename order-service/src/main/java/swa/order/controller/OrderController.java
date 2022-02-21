b4d484f34d0667f8ac28e2f04bf2bc348fb04664
ilkkamaksy <ijmakine@gmail.com>
2022-02-21 10:24:00 +0200

    refactor: global cors config

--- order-service/src/main/java/swa/order/controller/OrderController.java
+++ order-service/src/main/java/swa/order/controller/OrderController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,19 +25,16 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
     @PostMapping("orders")
     public CreditOrder createOrder(@Valid @RequestBody OrderRequest order) {
         return orderService.createOrder(order);
     }
 
-    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
     @GetMapping("orders")
     public List<CreditOrder> getAllOrders() {
         return orderService.getAll();
     }
 
-    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
     @GetMapping("orders/{id}")
     public CreditOrder getOrderById(@PathVariable Integer id) {
         return orderService.getOrderById(id);
